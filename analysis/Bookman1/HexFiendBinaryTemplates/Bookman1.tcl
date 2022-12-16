78695d3e0b93084f8beeea722cf849549f1c23e4
jsyang <jsyang.ca@gmail.com>
2022-12-16 13:15:10 +0000

    add binary template for findings so far

---
+++ analysis/Bookman1/HexFiendBinaryTemplates/Bookman1.tcl
@@ -0,0 +1,60 @@
+requires 0 "42 6F 6F 6B 6D 61 6E 31";
+
+section "Header" {
+    goto 0x0
+    ascii 8 "Magic number"
+
+    goto 0x48
+    uint32 "Bytesize U1"
+    uint32 "Bytesize U2"
+
+    goto 0x80
+    ascii 22 "Model info"
+}
+
+section "App Icon" {
+    goto 0x7E
+    uint16 "Struct addr"
+    move -2
+    set imgoffset [uint16]
+    goto $imgoffset
+    uint8 "Width"
+    move 1
+    uint8 "Height"
+    move 1
+    move 2
+    uint32 "Icon data addr"
+}
+
+section "SoftKeys" {
+    goto 0xAA
+    set fkeystart [uint16]
+    goto $fkeystart
+    cstr "ascii" "Func1"
+
+    goto 0xAC
+    set fkeystart [uint16]
+    goto $fkeystart
+    cstr "ascii" "Func2"
+
+    goto 0xAE
+    set fkeystart [uint16]
+    goto $fkeystart
+    cstr "ascii" "Func3"
+
+    goto 0xB0
+    set fkeystart [uint16]
+    goto $fkeystart
+    cstr "ascii" "Func4"
+}
+
+section "6502 Vectors" {
+    goto 0x7FFC
+    uint16 "RESET addr"
+
+    goto 0x7FFA
+    uint16 "IRQ addr"
+
+    goto 0x7FFE
+    uint16 "NMI addr"
+}
--- utils/Bookman1/constants.js
+++ utils/Bookman1/constants.js
@@ -12,24 +12,31 @@ const MAGIC_NUMBER = new Uint8Array([
     0x31    // 1
 ]);
 
-const OFFSET_ROM_FILESIZE = 0x48;
-// This UInt32LE is the total file size of the ROM
-const ROM_FILESIZE_SIZE = 4;        
+// These UInt32LE values represent the total file sizes of the ROM banks U1, U2
+const OFFSET_ROM_U1_FILESIZE = 0x48;
+const ROM_U1_FILESIZE_SIZE = 4;        
 
+const OFFSET_ROM_U2_FILESIZE = 0x4C;
+const ROM_U2_FILESIZE_SIZE = 4;        
 
+// Function keys (soft keys)
 const OFFSET_FUNCTION_KEY_1_NAME = 0xAA;
 const OFFSET_FUNCTION_KEY_2_NAME = 0xAC;
 const OFFSET_FUNCTION_KEY_3_NAME = 0xAE;
 const OFFSET_FUNCTION_KEY_4_NAME = 0xB0;
 
+// Images
 const OFFSET_CARD_ICON_STRUCT = 0x7E;
 const CARD_ICON_STRUCT_SIZE = 10; // bytes
 
 module.exports = {
     MAGIC_NUMBER,
 
-    OFFSET_ROM_FILESIZE,
-    ROM_FILESIZE_SIZE,
+    OFFSET_ROM_U1_FILESIZE,
+    ROM_U1_FILESIZE_SIZE,
+    
+    OFFSET_ROM_U2_FILESIZE,
+    ROM_U2_FILESIZE_SIZE,
     
     OFFSET_FUNCTION_KEY_1_NAME,
     OFFSET_FUNCTION_KEY_2_NAME,
